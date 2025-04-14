4552bf3d63da2c1833f3fe1e5195382b744a37ae
@Fuad <academicfile.info@gmail.com>
2025-04-14 15:09:41 +0600

    minor updates

--- mod19-array1/array_looping_tasks.js
+++ mod19-array1/array_looping_tasks.js
@@ -13,7 +13,7 @@
 
     4. Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
     Input: const statement = 'I am a hard working person'
-    'person working hard a am I'
+    Output: 'person working hard a am I'
 --------------------------------------------------------------------
 */
 
